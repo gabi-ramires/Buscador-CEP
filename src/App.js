998dd392a3556d0bc4ea358acd2eb72190d8e809
Gabriela Ramires <engenheira.ramires@gmail.com>
2022-06-15 23:51:00 -0300

    Primeira versão

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { FiSearch } from 'react-icons/fi';
+import './styles.css'
+import api from './services/api.js'
 
 function App() {
+
+ 
+
+
+const [ input, setInput ] = useState('') //input é o valor do estado, setInput é para passar o novo valor do estado
+
+const [cep, setCep] = useState({});
+
+async function Pesquisar(){ //função assincrona
+  if (input === ""){
+    alert("Preencha algum CEP")
+    return;
+  }
+
+  try{ //o que quer fazer, mas pode dar errado
+      const response = await api.get(`${input}/json`)
+      setCep(response.data)
+      setInput("")// deixa o input vazio
+  }
+
+  catch{
+    alert("Ops, não foi possível encontrar o CEP.")
+    setInput("")
+  }
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input
+        type="text"
+        placeholder="Digite seu cep"
+        value={input}
+        onChange={(evento) => setInput(evento.target.value) } // evento.target.value é o valor do input que está sendo mandado para o setInput
+        />
+
+        <div className="botao">
+        <button className="buttonSearch" onClick={Pesquisar}>
+          <FiSearch size={25} color="#FFF"/>
+        </button>
+        </div>
     </div>
+
+        {Object.keys(cep).length > 0 && (
+           <main className="main">
+          <h2>CEP: {cep.cep}</h2>
+          <span>{cep.logradouro}</span>
+          <span>Complemento: {cep.complemento}</span>
+          <span>{cep.bairro}</span>
+          <span>{cep.localidade} - {cep.uf}</span>
+
+        </main>
+        )
+        
+        }
+
+       
+
+    
+<div className="footer">Desenvolvido por: Gabriela Ramires | Para mais aplicações: <a href="https://portfolio-gabi.herokuapp.com/">www.portfolio-gabi.herokuapp.com</a></div>
+      
+    </div>
+    
   );
 }
 
